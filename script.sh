4fdace25d62bea48f43d1bc41ee7b4eee323bd9a
saiida1 <35665538+saiida1@users.noreply.github.com>
2018-07-04 14:09:23 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gnome-terminal -x sh -c "./gradlew jettyRunWar" ; sleep 10 ; curl -X POST --form pojo=@examples/pojo-server/gbm_3f258f27_f0ad_4520_b6a5_3d2bb4a9b0ff.java --form jar=@examples/pojo-server/h2o-genmodel.jar localhost:55000/makewar > example.war 
