4142cc66d2fd0bf146459f667f1b4cc16b1e1130
saiida1 <35665538+saiida1@users.noreply.github.com>
2018-07-09 11:06:39 +0100

    Update script.sh

--- prediction-service-builder/script.sh
+++ prediction-service-builder/script.sh
@@ -2,5 +2,5 @@
 #ps -aux | grep gradlew
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 export _JAVA_OPTIONS=-Xmx12096m
-(./gradlew jettyRunWar & sleep 10) ; curl -X POST --form pojo=@/home/saida/pojos/rf.java --form jar=@/home/saida/pojos/h2o-genmodel.jar localhost:55000/makewar > example.war ; sleep 120 
+(./gradlew jettyRunWar & sleep 10) ; curl -X POST --form pojo=@/home/saida/pojos/rf.java --form jar=@/home/saida/pojos/h2o-genmodel.jar localhost:55000/makewar > example.war ; sleep 180 
 exit 0
